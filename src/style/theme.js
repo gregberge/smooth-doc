701dd93d6540049298ffe7d1a48050295c100aa0
Morgane Dubus <30866152+mdubus@users.noreply.github.com>
2018-12-06 13:52:40 +0100

    fix: primary theme color (#3)

--- src/style/theme.js
+++ src/style/theme.js
@@ -2,7 +2,6 @@ import { theme as suTheme } from '@smooth-ui/core-sc'
 
 export default {
   ...suTheme,
-  primary: '#E27600',
   secondary: '#3D3D3D',
   subtitleColor: '#6d6d6d',
   editorTextColor: '#1b2b35',
