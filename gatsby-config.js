632974f6f6f206d4f3b9566ad54c6733a48dc9ae
Greg Bergé <greg@smooth-code.com>
2019-08-09 11:55:39 +0200

    feat: upgrade mdx & add support for live jsx (#5)

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ function config({
       'gatsby-plugin-styled-components',
       'gatsby-plugin-react-helmet',
       {
-        resolve: 'gatsby-mdx',
+        resolve: 'gatsby-plugin-mdx',
         options: {
           defaultLayouts: {
             default: require.resolve('./src/layouts/default'),
--- src/components/RootWrapper.js
+++ src/components/RootWrapper.js
@@ -1,41 +1,16 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
-import { th, styled, up, css } from '@smooth-ui/core-sc'
 import { ThemeProvider } from './ThemeProvider'
 import { GlobalStyle } from './GlobalStyle'
 import { Code } from './Code'
 
-const Editor = styled.div`
-  background-color: #f5f2f0;
-  color: ${th('editorTextColor')};
-  padding: 15px 20px;
-  margin: 25px -20px;
-  overflow: auto;
-  font-size: 14px;
-  line-height: 1.45;
-  border-radius: 3px;
-  border-radius: 0;
-  overflow-y: auto;
-
-  ${up(
-    'sm',
-    css`
-      border-radius: 3px;
-    `,
-  )}
-`
-
 const components = {
-  pre: ({
-    children: {
-      props: { className, children },
-    },
-  }) => {
+  code: ({ children, className, ...props }) => {
     const lang = className && className.split('-')[1]
     return (
-      <Editor>
-        <Code lang={lang}>{children}</Code>
-      </Editor>
+      <Code lang={lang} {...props}>
+        {children}
+      </Code>
     )
   },
 }
