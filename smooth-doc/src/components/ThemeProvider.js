f94650926e39906295b0ee4278b58b2604ae47be
Greg Bergé <greg@smooth-code.com>
2019-09-25 22:46:53 +0200

    feat: dark mode, better ad position

--- smooth-doc/src/components/ThemeProvider.js
+++ smooth-doc/src/components/ThemeProvider.js
@@ -1,20 +1,55 @@
 import React from 'react'
-import { theme, ThemeProvider as BaseThemeProvider } from '@smooth-ui/core-sc'
+import { transparentize } from 'polished'
+import {
+  ThemeProvider as SCThemeProvider,
+  ColorModeProvider,
+} from '@xstyled/styled-components'
+import { th } from '@xstyled/system'
 
 const baseTheme = {
-  ...theme,
-  secondary: '#3D3D3D',
-  subtitleColor: '#6d6d6d',
-  editorTextColor: '#1b2b35',
-  blockquoteColor: '#ffe564',
-  textColor: theme.gray800,
-  headerHeight: '122px',
+  initialColorModeName: 'light',
+  space: {},
+  transitions: {
+    base: '300ms ease',
+  },
+  sizes: {
+    'header-height': 122,
+  },
+  colors: {
+    bg: '#fff',
+    'secondary-bg': '#f7f7f7',
+    primary: '#bd4932',
+    border: '#ececec',
+    text: '#222',
+    'editor-text': '#403f53',
+    'editor-bg': '#f4f4f4',
+    blockquote: '#ffe564',
+    'blockquote-bg': p => transparentize(0.7, th.color('blockquote')(p)),
+    subtitle: '#6d6d6d',
+    'home-hero-img-bg': 'transparent',
+    'nav-link': th.color('text'),
+    'nav-link-hover': p => transparentize(0.3, th.color('nav-link')(p)),
+    'menu-button-shadow': p => transparentize(0.7, th.color('text')(p)),
+    modes: {
+      dark: {
+        primary: '#ff6042',
+        bg: '#000',
+        subtitle: '#eee',
+        'secondary-bg': '#111',
+        border: '#333',
+        'home-hero-img-bg': '#fff',
+        text: '#fff',
+        'editor-text': '#ddd',
+        'editor-bg': '#222',
+      },
+    },
+  },
 }
 
 export function ThemeProvider({ children, theme }) {
   return (
-    <BaseThemeProvider theme={{ ...baseTheme, ...theme }}>
-      {children}
-    </BaseThemeProvider>
+    <SCThemeProvider theme={{ ...baseTheme, ...theme }}>
+      <ColorModeProvider>{children}</ColorModeProvider>
+    </SCThemeProvider>
   )
 }
