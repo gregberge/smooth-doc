9e066351493d284c64b842db2411c16194ceff03
Greg Bergé <greg@smooth-code.com>
2019-09-26 11:59:08 +0200

    fix: support gatsby ssr color mode

--- smooth-doc/gatsby-ssr.js
+++ smooth-doc/gatsby-ssr.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { getColorModeInitScriptElement } from '@xstyled/styled-components'
 import { RootWrapper } from './src/components/RootWrapper'
 
 export const wrapRootElement = ({ element }) => (
   <RootWrapper>{element}</RootWrapper>
 )
+
+export function onRenderBody({ setPreBodyComponents }) {
+  setPreBodyComponents([getColorModeInitScriptElement()])
+}
