fd0a52103ebfe02c11a4afa31a536677dc85ef62
Greg Bergé <greg@smooth-code.com>
2019-09-26 16:28:00 +0200

    feat: support dark logo

--- smooth-doc/src/components/HomeHero.js
+++ smooth-doc/src/components/HomeHero.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { graphql, StaticQuery } from 'gatsby'
 import Img from 'gatsby-image'
-import styled from '@xstyled/styled-components'
+import styled, { useColorMode } from '@xstyled/styled-components'
 
 const QUERY = graphql`
   query HomeHero {
     logo: file(relativePath: { eq: "home-logo.png" }) {
+      childImageSharp {
+        fixed(width: 250, height: 250) {
+          ...GatsbyImageSharpFixed_noBase64
+        }
+      }
+    }
+
+    logoDark: file(relativePath: { eq: "home-logo-dark.png" }) {
       childImageSharp {
         fixed(width: 250, height: 250) {
           ...GatsbyImageSharpFixed_noBase64
@@ -40,21 +48,32 @@ const ImgContainer = styled.div`
   }
 `
 
+function getLogo(data, mode) {
+  if (mode === 'dark' && data.logoDark) return data.logoDark
+  return data.logo || null
+}
+
 export function HomeHero({ title, plainBg }) {
+  const [mode] = useColorMode()
+
   return (
     <StaticQuery
       query={QUERY}
-      render={data => (
-        <Container>
-          <ImgContainer data-plain-bg={plainBg}>
-            <Img
-              fixed={data.logo.childImageSharp.fixed}
-              alt={data.site.siteMetadata.title}
-            />
-          </ImgContainer>
-          <Title>{title}</Title>
-        </Container>
-      )}
+      render={data => {
+        const logo = getLogo(data, mode)
+        if (!logo) return null
+        return (
+          <Container>
+            <ImgContainer data-plain-bg={plainBg}>
+              <Img
+                fixed={logo.childImageSharp.fixed}
+                alt={data.site.siteMetadata.title}
+              />
+            </ImgContainer>
+            <Title>{title}</Title>
+          </Container>
+        )
+      }}
     />
   )
 }
