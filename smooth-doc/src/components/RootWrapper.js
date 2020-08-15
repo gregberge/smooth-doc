7bab24efa7eb8cc805c8d014364161179e88f9c4
Greg Bergé <greg@smooth-code.com>
2020-08-15 19:22:31 +0200

    fix: fix usage of "code" tag inside jsx

--- smooth-doc/src/components/RootWrapper.js
+++ smooth-doc/src/components/RootWrapper.js
@@ -5,14 +5,27 @@ import { ThemeProvider } from './ThemeProvider'
 import { GlobalStyle } from './GlobalStyle'
 import { Code } from './Code'
 
+function transformCode({ children, className, ...props }) {
+  const lang = className && className.split('-')[1]
+  return (
+    <Code lang={lang} {...props}>
+      {children}
+    </Code>
+  )
+}
+
+function getCodeChild(children) {
+  const childrenArray = React.Children.toArray(children)
+  if (childrenArray.length !== 1) return null
+  const [firstChild] = childrenArray
+  if (firstChild.props.mdxType !== 'code') return null
+  return firstChild
+}
+
 const components = {
-  code: ({ children, className, ...props }) => {
-    const lang = className && className.split('-')[1]
-    return (
-      <Code lang={lang} {...props}>
-        {children}
-      </Code>
-    )
+  pre: ({ children }) => {
+    const codeChild = getCodeChild(children)
+    return <pre>{codeChild ? transformCode(codeChild.props) : children}</pre>
   },
 }
 
