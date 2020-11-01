27b349ae4a06778ed3d0cb6e625100e767a4d209
David <david.bismut@gmail.com>
2020-11-01 20:15:36 +0100

    Import Gatsby Link

--- smooth-doc/src/components/MDX.js
+++ smooth-doc/src/components/MDX.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import { MDXProvider as BaseMDXProvider } from '@mdx-js/react'
 import { Code } from './Code'
 import { CarbonAd } from './CarbonAd'
