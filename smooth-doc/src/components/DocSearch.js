48bc1700fc0d5533341ac113dc27135fdaf363dd
Greg Bergé <greg@smooth-code.com>
2021-01-24 15:41:57 +0100

    feat(docsearch): use new version

--- smooth-doc/src/components/DocSearch.js
+++ smooth-doc/src/components/DocSearch.js
@@ -1,82 +1,168 @@
-import React from 'react'
-import { x, createGlobalStyle, th } from '@xstyled/styled-components'
-import { RiSearchEyeLine } from 'react-icons/ri'
+import * as React from 'react'
+import { Link, navigate } from 'gatsby'
+import { createPortal } from 'react-dom'
+import styled, { x, createGlobalStyle } from '@xstyled/styled-components'
+import { useDocSearchKeyboardEvents } from '@docsearch/react'
+import { RiSearchLine } from 'react-icons/ri'
 import { Input, InputGroup, InputGroupIcon } from './Input'
 
-const DocSearchStyle = createGlobalStyle`
-  .algolia-autocomplete .algolia-docsearch-suggestion--highlight {
-    color: doc-search-suggestion-highlight-on-background;
-    background-color: doc-search-suggestion-highlight-background;
-  }
+require('@docsearch/css')
 
-  .algolia-autocomplete .algolia-docsearch-suggestion--text .algolia-docsearch-suggestion--highlight {
-    box-shadow: inset 0 -2px 0 0 ${th.color(
-      'doc-search-suggestion-content-underline',
-    )};
-  }
-
-  .algolia-autocomplete .ds-cursor .algolia-docsearch-suggestion--content {
-    background-color: doc-search-suggestion-content-background !important;
-  }
-
-  .algolia-autocomplete .ds-dropdown-menu [class^=ds-dataset-] {
-    background-color: background;
-    border-color: layout-border;
-  }
-
-  .algolia-autocomplete .ds-dropdown-menu:before {
-    background-color: background;
-    border-color: layout-border;
-  }
-
-  .algolia-autocomplete .algolia-docsearch-suggestion {
-    background-color: background;
-    color: on-background;
-  }
-
-  .algolia-autocomplete .algolia-docsearch-suggestion--category-header {
-    border-color: layout-border;
-    color: on-background-light;
-  }
-
-  .algolia-autocomplete .algolia-docsearch-suggestion--title {
-    color: on-background;
-  }
-
-  .algolia-autocomplete .algolia-docsearch-suggestion--subcategory-column {
-    color: on-background-light;
-  }
-
-  .algolia-autocomplete .algolia-docsearch-suggestion--text {
-    color: on-background-light;
+const GlobalStyle = createGlobalStyle`
+  /* Darkmode */
+  body.xstyled-color-mode-dark {
+    --docsearch-text-color: rgb(245, 246, 247);
+    --docsearch-container-background: rgba(9, 10, 17, 0.8);
+    --docsearch-modal-background: rgb(21, 23, 42);
+    --docsearch-modal-shadow: inset 1px 1px 0 0 rgb(44, 46, 64),
+      0 3px 8px 0 rgb(0, 3, 9);
+    --docsearch-searchbox-background: rgb(9, 10, 17);
+    --docsearch-searchbox-focus-background: #000;
+    --docsearch-hit-color: rgb(190, 195, 201);
+    --docsearch-hit-shadow: none;
+    --docsearch-hit-background: rgb(9, 10, 17);
+    --docsearch-key-gradient: linear-gradient(
+      -26.5deg,
+      rgb(86, 88, 114) 0%,
+      rgb(49, 53, 91) 100%
+    );
+    --docsearch-key-shadow: inset 0 -2px 0 0 rgb(40, 45, 85),
+      inset 0 0 1px 1px rgb(81, 87, 125), 0 2px 2px 0 rgba(3, 4, 9, 0.3);
+    --docsearch-footer-background: rgb(30, 33, 54);
+    --docsearch-footer-shadow: inset 0 1px 0 0 rgba(73, 76, 106, 0.5),
+      0 -4px 8px 0 rgba(0, 0, 0, 0.2);
+    --docsearch-logo-color: rgb(255, 255, 255);
+    --docsearch-muted-color: rgb(127, 132, 151);
   }
 `
 
-export function DocSearch({ apiKey, indexName }) {
-  React.useEffect(() => {
-    window.docsearch({
-      apiKey,
-      indexName,
-      inputSelector: '#algolia-docsearch-input',
-      debug: true,
+function Hit({ hit, children }) {
+  return <Link to={hit.url}>{children}</Link>
+}
+
+const Kbd = styled.kbd`
+  border: 1;
+  border-color: control-border;
+  margin-right: 1;
+  background-color: control-background;
+  text-align: center;
+  padding: 0;
+  display: inline-flex;
+  justify-content: center;
+  font-size: 0.8em;
+  line-height: 1.2;
+  font-family: sans-serif;
+  border-radius: base;
+  min-width: 1.5em;
+`
+
+let DocSearchModal = null
+
+export const DocSearch = ({ apiKey, indexName }) => {
+  const searchButtonRef = React.useRef(null)
+  const [isShowing, setIsShowing] = React.useState(false)
+  const [initialQuery, setInitialQuery] = React.useState(null)
+
+  const importDocSearchModalIfNeeded = React.useCallback(() => {
+    if (DocSearchModal) {
+      return Promise.resolve()
+    }
+
+    return Promise.resolve(import('@docsearch/react/modal')).then(
+      ({ DocSearchModal: Modal }) => {
+        DocSearchModal = Modal
+      },
+    )
+  }, [])
+
+  const onOpen = React.useCallback(() => {
+    importDocSearchModalIfNeeded().then(() => {
+      // We check that no other DocSearch modal is showing before opening this
+      // one (we use one instance for desktop and one instance for mobile).
+      if (document.body.classList.contains('DocSearch--active')) {
+        return
+      }
+
+      setIsShowing(true)
     })
-  }, [apiKey, indexName])
+  }, [importDocSearchModalIfNeeded, setIsShowing])
+
+  const onClose = React.useCallback(() => {
+    setIsShowing(false)
+  }, [setIsShowing])
+
+  const onInput = React.useCallback(
+    (event) => {
+      importDocSearchModalIfNeeded().then(() => {
+        setIsShowing(true)
+        setInitialQuery(event.key)
+      })
+    },
+    [importDocSearchModalIfNeeded, setIsShowing, setInitialQuery],
+  )
+
+  useDocSearchKeyboardEvents({
+    isOpen: isShowing,
+    onOpen,
+    onClose,
+    onInput,
+    searchButtonRef,
+  })
 
   return (
     <>
-      <DocSearchStyle />
-      <x.div>
+      <GlobalStyle />
+      <div>
         <InputGroup>
           <InputGroupIcon>
-            <RiSearchEyeLine />
+            <RiSearchLine />
           </InputGroupIcon>
           <Input
-            id="algolia-docsearch-input"
+            ref={searchButtonRef}
+            onClick={onOpen}
             type="search"
             placeholder="Search..."
           />
+          <x.div
+            position="absolute"
+            top="50%"
+            right={0}
+            transform="translateY(-50%)"
+            display="inline-flex"
+            pointerEvents="none"
+            userSelect="none"
+          >
+            <Kbd>⌘</Kbd>
+            <Kbd>K</Kbd>
+          </x.div>
         </InputGroup>
-      </x.div>
+      </div>
+
+      {isShowing &&
+        createPortal(
+          <DocSearchModal
+            apiKey={apiKey}
+            indexName={indexName}
+            initialQuery={initialQuery}
+            onClose={onClose}
+            navigator={{
+              navigate({ suggestionUrl }) {
+                navigate(suggestionUrl)
+              },
+            }}
+            transformItems={(items) => {
+              return items.map((item) => {
+                const url = new URL(item.url)
+                return {
+                  ...item,
+                  url: item.url.replace(url.origin, ''),
+                }
+              })
+            }}
+            hitComponent={Hit}
+          />,
+          document.body,
+        )}
     </>
   )
 }
