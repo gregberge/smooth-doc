e6bc30d071b66137471bd7bd1c38d8b7c3016b2f
Greg Bergé <greg@smooth-code.com>
2021-01-24 17:40:10 +0100

    chore: fix website

--- website/gatsby-ssr.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/no-danger, import/no-extraneous-dependencies */
-const React = require('react')
-
-exports.onRenderBody = ({ setPostBodyComponents }) => {
-  setPostBodyComponents([
-    <script key="gumroad" async src="https://gumroad.com/js/gumroad.js" />,
-  ])
-}
