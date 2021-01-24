7c92c3c12a76f8581e066160754685910d954745
Greg Bergé <berge.greg@gmail.com>
2021-01-24 16:57:00 +0100

    feat(docsearch): use new version (#36)

--- smooth-doc/gatsby-ssr.js
+++ smooth-doc/gatsby-ssr.js
@@ -11,27 +11,6 @@ export const wrapPageElement = ({ element, props }) => {
   )
 }
 
-export const onRenderBody = (
-  { setHeadComponents, setPreBodyComponents, setPostBodyComponents },
-  { docSearch },
-) => {
+export const onRenderBody = ({ setPreBodyComponents }) => {
   setPreBodyComponents([getColorModeInitScriptElement()])
-
-  if (docSearch) {
-    setHeadComponents([
-      <link
-        key="plugin-docsearch-css"
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css"
-      />,
-    ])
-
-    setPostBodyComponents([
-      <script
-        key="plugin-docsearch-js"
-        type="text/javascript"
-        src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"
-      />,
-    ])
-  }
 }
